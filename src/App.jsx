a48677487c3f9c424e8a018e293aa139195cb15c
luanasj <luanasj@outlook.com.br>
2025-04-22 12:50:04 -0300

    feat: create title to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomePageTitle from './components/titles/HomePageTitle'
 
 function App() {
 
 
   return (
-   <h1 className='text-3x1 font-bold underline'>
-    Hello World
-    </h1>
+      <section className='z-50 max-w-[1156px] pt-[30px] pb-[30px] max-h-[537px] w-[90%] h-[90%] bg-black'>
+        <HomePageTitle/>
+      </section>
   )
 }
 
---
+++ src/components/icons/GithubName.jsx
@@ -0,0 +1,10 @@
+import github_name_icon from '/image 2.svg'
+
+const GithubName = ()=>{
+    return(
+        <img className="h-[48px] -mb-[5px]" src={github_name_icon} alt="" />
+
+    )
+}
+
+export default GithubName
