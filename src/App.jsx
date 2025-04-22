26956c7ac8faa873f73a8903749936ec483d31d6
luanasj <luanasj@outlook.com.br>
2025-04-22 19:01:37 -0300

    feat: create profile info display component

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ import HomePageTitle from './components/titles/HomePageTitle'
 import SearchBar from './components/forms/SearchBar'
 import ProfileCard from './components/cards/ProfileCard'
 import RequestError from './components/messages/requestError'
+import ProfileDisplay from './components/cards/ProfileDisplay'
 
 function App() {
 
 
   return (
-      <section className='z-50 max-w-[1156px] flex flex-col justify-start items-center gap-y-[22px] pt-[30px] pb-[30px] max-h-[537px] w-[90%] h-[90%] bg-black'>
+      <section className='z-50 max-w-[1156px] flex flex-col justify-start items-center gap-y-[30px] pt-[30px] pb-[30px] max-h-[537px] w-[90%] h-[90%] bg-black'>
         <HomePageTitle/>
         <SearchBar placeholder="Digite um usuário do Github"/>
         <ProfileCard>
           {/* <RequestError message="Nenhum perfil foi encontrado com esse nome de usuário."/> */}
+          <ProfileDisplay bio={"Oi! Eu sou uma Desenvolvedora de Software. (Software Developer)."} pictureURL={"https://avatars.githubusercontent.com/u/111328829?v=4"} userName={"Luana de Jesus"}/>
         </ProfileCard>
 
       </section>
--- src/components/cards/ProfileDisplay.jsx
+++ src/components/cards/ProfileDisplay.jsx
@@ -1,3 +1,13 @@
-const ProfileDisplay = ()=>{
-    
+const ProfileDisplay = ({pictureURL, userName, bio})=>{
+    return (
+    <div className="flex gap-x-8 items-center mr-[32px] my-4">
+        <img src={pictureURL} alt="user-picture" className="size-[220px] rounded-full  border-2 border-[#005CFF]"/>
+        <div className="flex flex-col gap-y-8 max-w-[448px]">
+            <h5 className="text-[20px] text-[#005CFF] font-['Nunito Sans'] font-bold h-7 leading-7">{userName}</h5>
+            <p className="font-['Nunito Sans'] font-light text-[15px] leading-[20px]">{bio}</p>
+        </div>
+    </div>
+    )
 }
+
+export default ProfileDisplay
