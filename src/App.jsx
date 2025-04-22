534170021d6e8ef85e21a83c1921f7bf138d3098
luanasj <luanasj@outlook.com.br>
2025-04-22 14:58:09 -0300

    feat: create search bar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
 import HomePageTitle from './components/titles/HomePageTitle'
+import SearchBar from './components/forms/SearchBar'
 
 function App() {
 
 
   return (
-      <section className='z-50 max-w-[1156px] pt-[30px] pb-[30px] max-h-[537px] w-[90%] h-[90%] bg-black'>
+      <section className='z-50 max-w-[1156px] flex flex-col justify-center items-center gap-y-[22px] pt-[30px] pb-[30px] max-h-[537px] w-[90%] h-[90%] bg-black'>
         <HomePageTitle/>
+        <SearchBar placeholder="Digite um usuário do Github"/>
       </section>
   )
 }
