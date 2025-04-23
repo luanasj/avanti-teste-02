3513bd3e6d4e6bab442813082572177342772b16
luanasj <luanasj@outlook.com.br>
2025-04-22 23:21:22 -0300

    refactor: changes content of messsagens component

--- src/components/messages/RequestError.jsx
+++ src/components/messages/RequestError.jsx
@@ -1,7 +1,7 @@
 const RequestError = ({message})=>{
     return(
         <p className="text-[20px] text-[#FF0000] font-normal mx-[60px] text-center leading-[27px]">
-            {message} <br /> Tente novamente
+            {message}
         </p>
     )
 }
