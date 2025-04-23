3cb246e430fdad7788e75bbf0caa8e3dc80b1d14
luanasj <luanasj@outlook.com.br>
2025-04-23 01:11:20 -0300

    add loading component

---
+++ src/components/icons/LoadingAnimation.jsx
@@ -0,0 +1,9 @@
+const LoadingAnimation = ()=>{
+    return(
+        <div class="flex items-center justify-center ">
+            <div class="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+        </div>
+    )
+}
+
+export default LoadingAnimation
